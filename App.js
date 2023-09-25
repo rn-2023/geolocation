d53d9dac9f3e49366f65df88e01b9aae6c6f9e3d
pekkao <pekka.ojala@oamk.fi>
2023-09-25 20:53:01 +0300

    Basic version

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+import React,{useState,useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setIsLoading(false);
+        console.log("Geolocation failed.");
+        return;
+      }
+  
+      const location = await Location.getCurrentPositionAsync({accuracy: Location.Accuracy.Lowest});
+      setLatitude(location.coords.latitude);
+      setLongitude(location.coords.longitude);
+      setIsLoading(false);      
+    })();
+  }, [])
+
+  if (isLoading) {
+    return <View style={styles.container}><Text>Retrieving location...</Text></View>
+  }
+  else {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <View style={styles.container}>
+        <Text>Location: </Text>
+        <Text>{latitude.toFixed(3)}</Text>
+        <Text>{longitude.toFixed(3)}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
